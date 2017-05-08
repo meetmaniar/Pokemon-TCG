940bef2492f83483d1ed891fce855f5a58fc3654
meetmaniar <meetrmaniar@gmail.com>
2017-05-07 21:07:20 -0400

    Create PriceCard.java

---
+++ PriceCard.java
@@ -0,0 +1,24 @@
+
+package pokemon;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+
+public class PriceCard extends JPanel {
+
+	public PriceCard() {
+		this.setLayout(new GridLayout(3 , 2));
+		this.setPreferredSize(new Dimension(32, 48));
+		
+		for(int i=0;i<6;i++) {
+			JPanel pc = new JPanel();
+			pc.setBorder(BorderFactory.createLineBorder(Color.black));
+			this.add(pc);
+		}
+	}
+}
