f89ece93843ee72930b10785258d63430d9c73b2
Maniar Meet <meetrmaniar@gmail.com>
2017-05-16 23:42:04 -0400

    added hitPoints , attackPoints , specialAttackPoints to the cards.

--- src/pokemon/Card.java
+++ src/pokemon/Card.java
@@ -1,3 +1,4 @@
+
 package pokemon;
 
 import java.awt.Color;
@@ -11,16 +12,29 @@ import javax.swing.JPanel;
 
 public class Card extends JPanel {
 
-	public Card(String name) {
+	public Card(String name,int hitPoints,int attackPoints, int specialAttackPoints) {
 		this.setPreferredSize(new Dimension(90, 90));
 		this.setBorder(BorderFactory.createLineBorder(Color.black));
 		
 		this.setLayout(null);
 		
 		JLabel nameLabel = new JLabel(name);
-		nameLabel.setBounds(3, 2, 700, 20);
+		nameLabel.setBounds(5, 2, 700, 20);
 		
+		JLabel hpLabel=new JLabel("HP:" +String.valueOf(hitPoints));
+		hpLabel.setBounds(5,30,700,20);
+		
+		JLabel apLabel=new JLabel("AP:" +String.valueOf(attackPoints));
+		apLabel.setBounds(5,45,700,20);
+		
+		JLabel sapLabel=new JLabel("SAP:" +String.valueOf(specialAttackPoints));
+		sapLabel.setBounds(0,60,700,20);
+		
+
 		this.add(nameLabel);
+		this.add(hpLabel);
+		this.add(apLabel);
+		this.add(sapLabel);
 	}
 	
 }
