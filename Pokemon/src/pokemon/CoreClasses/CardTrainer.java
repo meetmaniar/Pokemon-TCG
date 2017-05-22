1576056a1a1d394c6daeacd4b7af07677a892ce9
ZacChen11 <zacchenshuai@gmail.com>
2017-05-22 17:46:45 -0400

    changes for trainer

--- Pokemon/src/pokemon/CoreClasses/CardTrainer.java
+++ Pokemon/src/pokemon/CoreClasses/CardTrainer.java
@@ -6,17 +6,15 @@ public class CardTrainer extends Card {
 	public static final int TRAINER_ITEM 	= 2;
 
 	public int trainer_type;
-	public int heal_amount;
-	String name="";
+	public String name;
 	
 	public CardTrainer() {
 	super(2);
 	}
 	
-	public void initTrainer(String name,int trainer_type,int heal_amount)
+	public void initTrainer(String name,int trainer_type)
 	{
 		this.trainer_type=trainer_type;
-		this.heal_amount=heal_amount;
 		this.name=name;
 	}
 	
